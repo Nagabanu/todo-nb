ab85ce8172be85a597f5b158f3eade728b38dd76
bluntmachete <68460594+Nagabanu@users.noreply.github.com>
2023-01-03 09:09:15 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 def init_connection_engine():
     app.config['MYSQL_HOST'] = 'localhost'
     app.config['MYSQL_USER'] = 'root'
-    app.config['MYSQL_PASSWORD'] = 'root'
+    app.config['MYSQL_PASSWORD'] = ''
     app.config['MYSQL_DB'] = 'todo'
 
 
